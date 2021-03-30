0e73eb1946215ee926dbcda50b090aa927940afa
babas2 <79476483+babas2@users.noreply.github.com>
2021-03-30 11:10:13 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,16 +26,16 @@ except:
 @BotzHub.on(events.NewMessage(pattern="/start", func=lambda e: e.is_private))
 async def _(event):
     ok = await BotzHub(GetFullUserRequest(event.sender_id))
-    await event.reply(f"Salam {ok.user.first_name}! \nMən **Canlı Söhbət** botuyam.\nMənə bir mesaj göndər və məndə o mesajı saniyəsində aşağıdakı Kanal'a göndərəcəm!!",
+    await event.reply(f"Salam {ok.user.first_name} ! Mən Elan botuyam. Mənə satmaq istədiyin şeyin şəklini (qrup, kanal, əşya və s.) göndər məndə onu avtomatik aşağıdakı kanala yönləndirəcəm. **BAŞLAMADAN ƏVVƏL MÜTLƏQ /help YAZ** !",
                     buttons=[
                         [Button.url("🤖 Sahibim", url="https://t.me/c9ala"),
-                        Button.url("🗣️ Kanal", url="https://t.me/sohbetcanli")]
+                        Button.url("🗣️ Kanal", url="https://t.me/elanver")
                     ])
 
 @BotzHub.on(events.NewMessage(pattern="/help", func=lambda e: e.is_private))
 async def _(event):
     ok = await BotzHub(GetFullUserRequest(event.sender_id))
-    await event.reply(f"{ok.user.first_name} kömək menyusuna xoş gəldin!\n\n🤖 Bot haqqında qısaca məlumat:\nSən mənə istədiyin bir mesajı yazırsan məndə həmin saniyə o mesajı @SohbetCanli kanalına yönləndirirəm! Sənin mesajını görən insanlarda bot vaistəsi ilə sənə cavab verəcək.\n\nTəklif və Şikayətlər üçün /feedback yaza bilərsiz.",
+    await event.reply(f"{ok.user.first_name} kömək menyusuna xoş gəldin!\n\n🤖 Bot haqqında qısaca məlumat:\nSən mənə satmaq istədiyin şeyin şəklini (qrup, kanal, əşya və s.) göndərirsən və məndə onu avtomatik @ElanVer kanalına yönləndirirəm.\n\n**VACİB**:\n1 - Elanını paylaşarkən onun haqqında tam məlumat verməyi unutma (ünvan, qiymət, əlaqə və s.)\n2 - 18+ ,qanunsuz, qeyri-etik elanlar paylaşmaq qadağandır.\n\n✍️ Qeyd: Elan paylaşılarkən kim tərəfindən göndərildiyi kanalda göstərilir, yəniki nələr paylaşdığın sənin "tərbiyyəndən" aslıdır :)",
                     buttons=[
                         Button.url("🗣️ Kanal", url="https://t.me/sohbetcanli")
                     ])
